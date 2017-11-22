c39c154454448232a261464d1e320bfc7be093b9
Coronel Mustang <Coronel Mustang@DESKTOP-SNE54S7>
2017-11-22 13:09:32 -0200

    Inicializando login

---
+++ src/java/br/com/appcontabil/usuario/Usuario.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.appcontabil.usuario;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Coronel Mustang
+ */
+@Named
+@ViewScoped
+public class Usuario {
+    
+    private int id_usuario;
+    private String usuario;
+    private String login;
+    private String senha;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
